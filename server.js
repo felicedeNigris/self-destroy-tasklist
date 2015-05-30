2abde36196628f3bb68001592e6a56a1d8e4412b
Felice DeNigris <felideni@gmail.com>
2015-05-29 21:52:17 -0400

    Added server.js and node

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var http = require("http"),
-    server;
+var http = require('http'), server;
 
-server = http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World!\n");
-});
+server = http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello World</h1>');
+  });
+
+var port = Number(process.env.PORT || 3000);
 
 server.listen(3000);
-
-console.log("Server running on port 3000");
