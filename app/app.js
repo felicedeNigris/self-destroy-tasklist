108750ca73095b32f810b23cd4dd0abc9549eaaf
Felice DeNigris <felideni@gmail.com>
2015-06-05 20:47:46 -0400

    Added basic html and angular to the app

--- app/app.js
+++ app/app.js
@@ -1,3 +1,20 @@
+//console.log("Loaded Module!");
+
+
 var taskApp = angular.module('taskApp', [ ]);
+
+
+
+
+
+  taskApp.directive('addTask', function(){
+    return{
+      restrict: "E",
+      template: "Farting"
+    };
+
+  }); // closing tags for addTask directive
+
+
 
 
