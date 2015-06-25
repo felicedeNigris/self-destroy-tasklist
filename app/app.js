e8e949edfabe8db699d9c8e00acb927c75a8d5c7
Felice DeNigris <felideni@gmail.com>
2015-06-25 00:43:31 -0400

    still trying services

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
-var ToDo = angular.module('ToDo',['ngRoute','firebase']);
+(function(angular){
+    'use strict';
+
+var ToDo = angular.module('ToDo',['ngRoute','firebase','DataFactory']);
 
 
     ToDo.config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {
@@ -17,87 +20,31 @@ var ToDo = angular.module('ToDo',['ngRoute','firebase']);
       
     }]);
 
-    Todo.value('fbURL', "https://mia-lista.firebaseio.com/ToDos") //Firebase URL value service
-    .service('fbRef',function(fbURL){ //Firebase Data Reference Service
-      return new Firebase(fbURL);
-    })
-    .service('fbArr',function(fbRef){ //Firebase Array service
-      $scope.todos = $firebaseArray(fbRef);
-      return $scope.todos;
-    });
 
 
-
-    ToDo.controller('todoController',["$scope","$firebaseArray", "fbArr", function($scope, $firebaseArray, fbArr){ // injecting AngularFire & ToDoData Service 
+    ToDo.controller('todoController', function($scope, $firebaseArray, DataFactory){ // injecting AngularFire & ToDoData Service 
  
-      
-      //var myData = new Firebase("https://mia-lista.firebaseio.com/ToDos"); //create Firebase obj    COMMENT OUT BECAUSE THESE 2 LINES ARE IN SERVICE
-      //$scope.todos = $firebaseArray(fbRef); //Reading Database and adding to todos variable 
 
-      //return $scope.todos;
-
-      //$scope.historytodos = [{'title': "Old Task", 'done':true, 'timetag':new Date().toString()}];
-
-      $scope.addTodo = function(){
-
-        var datecreated = new Date().toString();
-        
-        $scope.todos.$add({'title':$scope.newtodo,'done':false, 'timetag': datecreated}); //push to Array 
-  
-        $scope.newtodo = '';
-      };
-      
-
-      $scope.clearCompleted = function(){  // clear completed tasks
-        $scope.todos.forEach(function(todo){ //for each todo if 'todo.done' , remove it.
-          if(todo.done){
-            $scope.todos.$remove(todo);
-          }
-        });
+      $scope.addTodo = function(newtodo){
+        DataFactory.addNewTodo(newtodo);
       };
 
+      $scope.clearCompleted = function(todo){
+        DataFactory.clearFinished(todo);
+      };
 
       $scope.clickDone = function(todo){
-        $scope.todos.$save(todo); //saves todo state when is checked
-        $scope.historytodos.push(todo); //push to history array 
-        //debugger;
+        DataFactory.markAsDone(todo);
       };
 
-      $scope.clearByDay = function(){  // clear completed tasks
-        var today = new Date().getUTCDate();
-        $scope.todos.forEach(function(todo){ //if todo is greater or equal to 2 delete
-          if( today - todo.timetag.getUTCDate() >=2){
-            $scope.todos.$remove(todo);
-          }
-        });
-      };
-
-     
       $scope.moveUpPriority = function($index){
-        var toMove = $scope.todos[$index]; //save copy of $index todo
-        
-       $scope.todos.splice($index,1); //removes item at $index 
-       $scope.todos.splice(0,0, toMove); //adds item toMove to the start of array
+        DataFactory.Priority($index);
+      };
 
-       var backup = $scope.todos; //copy back up array
+    });
 
-
-       for(var b = 0; b<= $scope.todos.length; b++){
-        $scope.todos.$remove($scope.todos[b]); //remove items from array
-       }
-
-       for(var i = 0; i<= backup.length; i++){
-        $scope.todos.$add(backup[i]); // add items from back up array
-       }
-     };
-     /*
-     $scope.addToHistory = function(todo){
-      if(todo.done){
-      $scope.historytodos.push(todo);
-        }
-     };
-    */
+})(window.angular);
 
 
-    }]);
+
  
--- app/services/services.js
+++ app/services/services.js
@@ -1,13 +1,55 @@
 
-Todo.value('fbURL', "https://mia-lista.firebaseio.com/ToDos")
-.service('fbRef',function(fbURL){
-  return new Firebase(fbURL);
-});
 
+var ToDo = angular.module('ToDo',['ngRoute','firebase']);
 
-//var myData = new Firebase("https://mia-lista.firebaseio.com/ToDos"); //create Firebase obj
-  $scope.todos = $firebaseArray(myData); //Reading Database and adding to todos variable 
-  return $scope.todos;
+ToDo.factory('DataFactory', [ "$firebaseArray", "$scope" ,function($scope, $firebaseArray){
 
+      var myData = new Firebase("https://mia-lista.firebaseio.com/ToDos"); //create Firebase obj
 
+      var todos = $firebaseArray(myData); //Reading Database & adding to todos variable
 
+      return{
+
+          getTodos : function(todos){
+            return $scope.todos;
+          },
+          
+          addNewTodo : function(newtodo){
+            var datecreated = new Date().toString();
+            $scope.todos.$add({'title':$scope.newtodo,'done':false, 'timetag': datecreated}); //push to array
+            $scope.newtodo='';
+          },
+
+          clearFinished : function(todo){ //clear completed tasks
+            $scope.todos.forEach(function(todo){ //for each todo if 'todo.done' , remove it.
+              if(todo.done){
+                $scope.todos.$remove(todo);
+                }
+              }
+            );
+          },
+
+          markAsDone : function(todo){
+            $scope.todos.$save(todo); //saves todo state when is checked
+            $scope.historytodos.push(todo); //push to history array 
+            
+          },
+
+          Priority : function($index){
+            var toMove = $scope.todos[$index]; //save copy of $index todo
+          
+             $scope.todos.splice($index,1); //removes item at $index 
+             $scope.todos.splice(0,0, toMove); //adds item toMove to the start of array
+
+             var backup = $scope.todos; //copy back up array
+
+             for(var b = 0; b<= $scope.todos.length; b++){
+              $scope.todos.$remove($scope.todos[b]); //remove items from array
+             }
+             for(var i = 0; i<= backup.length; i++){
+              $scope.todos.$add(backup[i]); // add items from back up array
+             }
+         },
+       };
+    }
+]);
