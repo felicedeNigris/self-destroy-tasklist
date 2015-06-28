d555bd59f878138fc25f15a65cc2e04bc9e3f558
Felice DeNigris <felideni@gmail.com>
2015-06-28 14:22:26 -0400

    Created Services that interact w Firebase

--- app/app.js
+++ app/app.js
@@ -1,8 +1,63 @@
-(function(angular){
-    'use strict';
 
-var ToDo = angular.module('ToDo',['ngRoute','firebase','DataFactory']);
+var ToDo = angular.module('ToDo',['ngRoute','firebase']);
 
+//SERVICE
+
+ToDo.service("TodoService",function($firebaseArray){
+
+  var ref = new Firebase("https://mia-lista.firebaseio.com/ToDos"); //create Firebase obj
+  
+  var todoArray = $firebaseArray(ref); //add (private variable)
+  
+  this.getTodos = function(){
+    return todoArray; //Reading Database & adding to todos variable
+  };
+
+  this.addTodo = function(newtodo){
+    return todoArray.$add(newtodo); //push to array
+  };
+
+  this.updateTodo = function(todo){
+    return todoArray.$save(todo); // saves todo state (as done or not done)
+  };
+
+  this.clearTodo = function(){
+  
+    todoArray.forEach(function(todo){ //for each todo if 'todo.done' , remove it.
+      if(todo.done){
+        todoArray.$remove(todo);
+        }
+      }
+    );
+  };
+});
+
+
+//  var datecreated = new Date().toString(); //date obj
+/*
+.factory("PriorityFactory", function(TodoFactory){
+  function Priority($index){
+  var toMove = todos[$index]; //save copy of $index todo
+          
+    todos.splice($index,1); //removes item at $index 
+    todos.splice(0,0, toMove); //adds item toMove to the start of array
+
+    var backup = todos; //copy back up array
+
+    for(var b = 0; b<= todos.length; b++){
+    todos.$remove(todos[b]); //remove items from array
+    }
+    for(var i = 0; i<= backup.length; i++){
+    todos.$add(backup[i]); // add items from back up array
+    }
+  }
+  return Priority($index); 
+});
+*/
+
+
+
+// CONFIG
 
     ToDo.config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {
       $routeProvider.when('/',{
@@ -22,28 +77,33 @@ var ToDo = angular.module('ToDo',['ngRoute','firebase','DataFactory']);
 
 
 
-    ToDo.controller('todoController', function($scope, $firebaseArray, DataFactory){ // injecting AngularFire & ToDoData Service 
- 
+//CONTROLLERS
 
-      $scope.addTodo = function(newtodo){
-        DataFactory.addNewTodo(newtodo);
+    ToDo.controller('todoController',['$scope','TodoService',function($scope,TodoService){ // injecting AngularFire & Factory
+      
+      $scope.todos = TodoService.getTodos();
+      
+      $scope.newtodo = {'title':'','done':false,'timetag':new Date().toString()};
+      
+      $scope.addTodo = function(){
+        console.log($scope.newtodo);
+        TodoService.addTodo($scope.newtodo); //add title to todo array
       };
 
       $scope.clearCompleted = function(todo){
-        DataFactory.clearFinished(todo);
+        TodoService.clearTodo(todo);
       };
 
       $scope.clickDone = function(todo){
-        DataFactory.markAsDone(todo);
+        todo.done = true;
+        TodoService.updateTodo(todo); //updates todo state
       };
-
+      /*
       $scope.moveUpPriority = function($index){
-        DataFactory.Priority($index);
+        TodoService.Priority($index);
       };
-
-    });
-
-})(window.angular);
+      */
+    }]);
 
 
 
--- app/services/services.js
+++ app/services/services.js
@@ -1,8 +1,8 @@
 
 
-var ToDo = angular.module('ToDo',['ngRoute','firebase']);
+var ToDo = angular.module('ToDo',[]);
 
-ToDo.factory('DataFactory', [ "$firebaseArray", "$scope" ,function($scope, $firebaseArray){
+ToDo.service('DataFactory', ["$firebase", "$firebaseArray", "$scope" ,function($scope, $firebase, $firebaseArray){
 
       var myData = new Firebase("https://mia-lista.firebaseio.com/ToDos"); //create Firebase obj
 
@@ -10,44 +10,44 @@ ToDo.factory('DataFactory', [ "$firebaseArray", "$scope" ,function($scope, $fire
 
       return{
 
-          getTodos : function(todos){
-            return $scope.todos;
+          getTodos : function(){
+            return todos;
           },
           
           addNewTodo : function(newtodo){
             var datecreated = new Date().toString();
-            $scope.todos.$add({'title':$scope.newtodo,'done':false, 'timetag': datecreated}); //push to array
+            todos.$add({'title':$scope.newtodo,'done':false, 'timetag': datecreated}); //push to array
             $scope.newtodo='';
           },
 
           clearFinished : function(todo){ //clear completed tasks
-            $scope.todos.forEach(function(todo){ //for each todo if 'todo.done' , remove it.
+            todos.forEach(function(todo){ //for each todo if 'todo.done' , remove it.
               if(todo.done){
-                $scope.todos.$remove(todo);
+                todos.$remove(todo);
                 }
               }
             );
           },
 
           markAsDone : function(todo){
-            $scope.todos.$save(todo); //saves todo state when is checked
-            $scope.historytodos.push(todo); //push to history array 
+            todos.$save(todo); //saves todo state when is checked
+            //$scope.historytodos.push(todo); //push to history array 
             
           },
 
           Priority : function($index){
-            var toMove = $scope.todos[$index]; //save copy of $index todo
+            var toMove = todos[$index]; //save copy of $index todo
           
-             $scope.todos.splice($index,1); //removes item at $index 
-             $scope.todos.splice(0,0, toMove); //adds item toMove to the start of array
+             todos.splice($index,1); //removes item at $index 
+             todos.splice(0,0, toMove); //adds item toMove to the start of array
 
-             var backup = $scope.todos; //copy back up array
+             var backup = todos; //copy back up array
 
-             for(var b = 0; b<= $scope.todos.length; b++){
-              $scope.todos.$remove($scope.todos[b]); //remove items from array
+             for(var b = 0; b<= todos.length; b++){
+              todos.$remove(todos[b]); //remove items from array
              }
              for(var i = 0; i<= backup.length; i++){
-              $scope.todos.$add(backup[i]); // add items from back up array
+              todos.$add(backup[i]); // add items from back up array
              }
          },
        };
